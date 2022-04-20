adc82b6a7077a1fa0387935c3d21b15479585e05
AliaksandrHancharou <A.Hancharou@outlook.com>
2022-04-20 10:57:44 +0300

    Add task HomeWork Task №2

--- homeWork7.py
+++ homeWork7.py
@@ -1,11 +1,24 @@
-palindrome_tuple = ('дед', 'морковь', 'яблоко', 'пейзаж', 'потоп', 'топот', 'рябина', 'шалаш')
+# palindrome_tuple = ('дед', 'морковь', 'яблоко', 'пейзаж', 'потоп', 'топот', 'рябина', 'шалаш')
 
-def palindrome(words: tuple) -> tuple:
-    if words == words[::-1]:
-        return True
-    else:
-        return False
+# def palindrome(words: tuple) -> tuple:
+#     if words == words[::-1]:
+#         return True
+#     else:
+#         return False
 
-palindrome_filter = filter(palindrome, palindrome_tuple)
+# palindrome_filter = filter(palindrome, palindrome_tuple)
 
-print(list(palindrome_filter))
+# print(list(palindrome_filter))
+
+
+#   №1
+some_words = list(map(str, input().split()))
+some_words[0], some_words[1] = some_words[1], some_words[0]
+delimiter = ' ! '
+print(f'!{some_words[0]} {delimiter} {some_words[1]}!')
+
+#   №2
+print('!{} ! {}!'.format(some_words[0], some_words[1]))
+
+#   №3
+print('!%s ! %s!' % (some_words[0], some_words[1]))
